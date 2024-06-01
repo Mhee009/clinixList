67bcdab1eeb1e11d365c999a86345ca7c68d0c97
mhee <mheeprising@gmail.com>
2024-06-01 17:17:14 +0700

    change style reg and login

--- register_form.php
+++ register_form.php
@@ -8,16 +8,16 @@
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<section class="vh-100" style="background-color: #eee;">
+<section class="vh-100 my-5" >
   <div class="container h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-lg-12 col-xl-11">
-        <div class="card text-black" style="border-radius: 25px;">
+        <div class="card text-black bg-warning" style="border-radius: 25px;">
           <div class="card-body p-md-5">
             <div class="row justify-content-center">
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">สร้างบัญชีของคุณ</p>
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">สร้างบัญชี</p>
 
                 <form class="mx-1 mx-md-4" method="POST" action="check_regis.php" enctype="multipart/form-data">
 
@@ -82,7 +82,7 @@
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
-                      <textarea rows="4" cols="50"></textarea>
+                      <textarea rows="4" cols="45"></textarea>
                       <label class="form-label">ที่อยู่คลินิคหรือโรงพยาบาลสัตว์</label>
                     </div>
                   </div>
@@ -119,17 +119,17 @@
                   </div>
 
                   <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                    <button type="button" class="btn btn-primary btn-lg">Register</button>
+                    <button type="button" class="btn btn-primary btn-lg">ลงทะเบียน</button>
                   </div>
 
                 </form>
 
               </div>
-              <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+              <!--<div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">-->
 
-                <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-registration/draw1.png" class="img-fluid" alt="Sample image">
+               <!-- <img src="img/img4.webp" class="img-fluid" alt="Sample image">-->
 
-              </div>
+              <!--</div>-->
             </div>
           </div>
         </div>
